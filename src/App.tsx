8be61b5f8ef0c4275febb58c4ed543a1dae08fa4
CharlesRajaR <rcr2003rcharlesraja@gmail.com>
2025-04-15 20:27:44 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,20 @@ import React from 'react';
 import './App.css';
 import { ThemeProvider } from '@emotion/react';
 import customTheme from './Theme/CustomTheme.ts';
-import Navbar from './Customer/Components/Navbar/Navbar.tsx';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Home from './Customer/Pages/Home/Home.tsx';
-import Product from './Customer/Pages/Product/Product.tsx';
-import ProductDetails from './Customer/Pages/ProductDetails/ProductDetails.tsx';
-import Cart from './Customer/Pages/Cart/Cart.tsx';
-import Checkout from './Customer/Pages/Checkout/Checkout.tsx';
-import Accounts from './Customer/Pages/Accounts/Accounts.tsx';
+import { Route, Router, Routes } from 'react-router-dom';
+import SellerRouter from './Seller/SellerRouter.tsx';
+import CustomerRouter from './Customer/CustomerRouter.tsx';
 
 function App() {
   return (
     <ThemeProvider theme={customTheme}>
       <div>
-        <Navbar/>
-        {/* <Home/> */}
-        {/* <Product/> */}
-        {/* <ProductDetails/> */}
-        {/* <Cart/> */}
-        {/* <Checkout/> */}
-        <Accounts/>
+           <Routes>
+            <Route path='/' element={<CustomerRouter/>}/>
+            <Route path='/seller' element={<SellerRouter/>}/>
+           </Routes>
       </div>
     </ThemeProvider>
   );
